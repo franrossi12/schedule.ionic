8e73d45feae2cd85cfe72d781ee4323e11bd96a8
Unknown <rossifrancisco12@gmail.com>
2018-09-10 20:43:01 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {TodayPage} from "../pages/today/today";
+import {CalendarPage} from "../pages/calendar/calendar";
 
 @NgModule({
   declarations: [
     MyApp,
+    TodayPage,
+    CalendarPage,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    TodayPage,
+    CalendarPage,
     AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
---
+++ src/pages/calendar/calendar.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-calendar',
+  templateUrl: 'calendar.html',
+})
+export class CalendarPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CalendarPage');
+  }
+
+}
